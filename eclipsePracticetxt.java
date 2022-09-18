cb322d6d02ee536a006faa6ea9febbe7dca283bf
KyleGibson6Code <kylegibson3gs@gmail.com>
2022-09-18 19:24:23 -0400

    System Print Practice

---
+++ eclipsePracticetxt.java
@@ -0,0 +1,16 @@
+package eclipsePracticeWork;
+
+public class eclipsePracticetxt {
+	public static void main(String args[]) {
+System.out.println("Kyle Gibson enjoys coding");
+System.out.println("Hi");
+System.out.print("Testing on adding words without using ln");
+System.out.print("Seeing where this line will go");
+System.out.println("I now see where the lines will go");
+System.out.print("Testing again");
+System.out.print("Apples");
+	}
+	
+
+}
+
